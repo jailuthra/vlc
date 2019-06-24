c7e88072b7f065fb314383a1809dcc76a3dc7a51
Jai Luthra <me@jailuthra.in>
2019-06-24 15:41:04 -0700

    nvdec: (temp) move to m/codec

--- modules/codec/nvdec.c
+++ modules/codec/nvdec.c
@@ -34,28 +34,28 @@
 typedef struct nvdec_ctx {
     CUvideodecoder cudecoder;
     CUvideoparser cuparser;
-    int num_surfaces;
+    int i_nb_surface;
 } nvdec_ctx_t;
 
-static int CUDAAPI HandleVideoSequence(void *opaque, CUVIDEOFORMAT *format)
+static int CUDAAPI HandleVideoSequence(void *p_opaque, CUVIDEOFORMAT *p_format)
 {
-    decoder_t *p_dec = (decoder_t *) opaque;
+    decoder_t *p_dec = (decoder_t *) p_opaque;
     nvdec_ctx_t *p_ctx = p_dec->p_sys;
 
     // post processed output's dimensions need to be 2-aligned in NVDEC
-    p_dec->fmt_out.video.i_width = (format->coded_width + 1) & ~1;
-    p_dec->fmt_out.video.i_height = (format->coded_height + 1) & ~1;
+    p_dec->fmt_out.video.i_width = (p_format->coded_width + 1) & ~1;
+    p_dec->fmt_out.video.i_height = (p_format->coded_height + 1) & ~1;
 
     CUVIDDECODECREATEINFO dparams = {0};
-    dparams.ulWidth             = format->coded_width;
-    dparams.ulHeight            = format->coded_height;
+    dparams.ulWidth             = p_format->coded_width;
+    dparams.ulHeight            = p_format->coded_height;
     dparams.ulTargetWidth       = p_dec->fmt_out.video.i_width;
     dparams.ulTargetHeight      = p_dec->fmt_out.video.i_height;
-    dparams.bitDepthMinus8      = format->bit_depth_luma_minus8;
+    dparams.bitDepthMinus8      = p_format->bit_depth_luma_minus8;
     dparams.OutputFormat        = cudaVideoSurfaceFormat_NV12;
-    dparams.CodecType           = format->codec;
-    dparams.ChromaFormat        = format->chroma_format;
-    dparams.ulNumDecodeSurfaces = p_ctx->num_surfaces;
+    dparams.CodecType           = p_format->codec;
+    dparams.ChromaFormat        = p_format->chroma_format;
+    dparams.ulNumDecodeSurfaces = p_ctx->i_nb_surface;
     dparams.ulNumOutputSurfaces = 1;
 
     CUresult result = cuvidCreateDecoder(&p_ctx->cudecoder, &dparams);
@@ -67,12 +67,12 @@ static int CUDAAPI HandleVideoSequence(void *opaque, CUVIDEOFORMAT *format)
     return 1;
 }
 
-static int CUDAAPI HandlePictureDecode(void *opaque, CUVIDPICPARAMS *params)
+static int CUDAAPI HandlePictureDecode(void *p_opaque, CUVIDPICPARAMS *p_picparams)
 {
-    decoder_t *p_dec = (decoder_t *) opaque;
+    decoder_t *p_dec = (decoder_t *) p_opaque;
     nvdec_ctx_t *p_ctx = p_dec->p_sys;
 
-    CUresult result = cuvidDecodePicture(&p_ctx->cudecoder, params);
+    CUresult result = cuvidDecodePicture(&p_ctx->cudecoder, p_picparams);
 
     if (result != CUDA_SUCCESS) {
         msg_Err(p_dec, "Could not create decoder object");
@@ -81,8 +81,62 @@ static int CUDAAPI HandlePictureDecode(void *opaque, CUVIDPICPARAMS *params)
     return 1;
 }
 
-static int CUDAAPI HandlePictureDisplay(void *opaque, CUVIDPARSERDISPINFO *dispinfo)
+static int CUDAAPI HandlePictureDisplay(void *p_opaque, CUVIDPARSERDISPINFO *p_dispinfo)
 {
+    decoder_t *p_dec = (decoder_t *) p_opaque;
+    nvdec_ctx_t *p_ctx = p_dec->p_sys;
+
+    CUdeviceptr cu_frame = 0;
+    unsigned char *p_raw;
+    unsigned int i_pitch;
+    CUVIDPROCPARAMS params;
+
+    memset(&params, 0, sizeof(params));
+    params.progressive_frame = p_dispinfo->progressive_frame;
+    params.top_field_first = p_dispinfo->top_field_first;
+
+    // Map decoded frame to a device pointer
+    CUresult result = cuvidMapVideoFrame(&p_ctx->cudecoder, p_dispinfo->picture_index,
+                                         &cu_frame, &i_pitch, &params);
+    if (result != CUDA_SUCCESS) {
+        msg_Err(p_dec, "Could not map frame");
+        return 0;
+    }
+
+    // Copy decoded frame into a new VLC picture
+    picture_t * p_pic = decoder_NewPicture(p_dec);
+    p_pic->b_progressive = params.progressive_frame;
+    p_pic->b_top_field_first = params.top_field_first;
+    p_pic->date = p_dispinfo->timestamp;
+
+    int i_plane_offset = 0;
+    for (int plane = 0; plane < p_pic->i_planes; plane++) {
+        plane_t plane = p_pic->p[plane];
+        CUDA_MEMCPY2D cu_cpy = {
+            .srcMemoryType  = CU_MEMORYTYPE_DEVICE,
+            .dstMemoryType  = CU_MEMORYTYPE_HOST,
+            .srcDevice      = cu_frame,
+            .srcY           = i_plane_offset,
+            .srcPitch       = i_pitch,
+            .dstHost        = plane.p_pixels,
+            .dstPitch       = plane.i_pitch,
+            .WidthInBytes   = __MIN(i_pitch, plane.i_pitch),
+            .Height         = plane.i_lines,
+        };
+        result = cuMemcpy2D(&cu_cpy);
+        if (result != CUDA_SUCCESS) {
+            msg_Err(p_dec, "Could not copy frame to display memory");
+            return 0;
+        }
+        i_plane_offset += cu_cpy.Height;
+    }
+    
+    // Release surface on GPU
+    result = cuvidUnmapVideoFrame(&p_ctx->cudecoder, &cu_frame);
+    if (result != CUDA_SUCCESS) {
+        msg_Err(p_dec, "Could not unmap frame");
+        return 0;
+    }
     return 1;
 }
 
@@ -124,6 +178,8 @@ static int OpenDecoder(vlc_object_t *p_this)
     if (p_dec->fmt_in.i_codec != VLC_CODEC_H264)
         return VLC_EGENERIC;
 
+    p_dec->fmt_out.i_codec = p_dec->fmt_out.video.i_chroma = VLC_CODEC_NV12;
+
     CUVIDDECODECAPS caps = {0};
     caps.eCodecType         = cudaVideoCodec_H264;
     caps.eChromaFormat      = cudaVideoSurfaceFormat_NV12;
@@ -134,13 +190,13 @@ static int OpenDecoder(vlc_object_t *p_this)
         return VLC_EGENERIC;
     }
 
-    p_ctx->num_surfaces = MAX_SURFACES;
+    p_ctx->i_nb_surface = MAX_SURFACES;
 
     CUVIDPARSERPARAMS pparams = {0};
     pparams.CodecType               = cudaVideoCodec_H264;
     pparams.ulClockRate             = 1e6;
     pparams.ulMaxDisplayDelay       = 4;
-    pparams.ulMaxNumDecodeSurfaces  = p_ctx->num_surfaces;
+    pparams.ulMaxNumDecodeSurfaces  = p_ctx->i_nb_surface;
     pparams.pUserData               = p_dec;
     pparams.pfnSequenceCallback     = HandleVideoSequence;
     pparams.pfnDecodePicture        = HandlePictureDecode;
