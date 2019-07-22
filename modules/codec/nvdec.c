cc76b72b4b02943c047b94dd8ac81e282655f00b
Jai Luthra <me@jailuthra.in>
2019-07-22 23:10:09 +0530

    nvdec: fix include order

--- modules/codec/nvdec.c
+++ modules/codec/nvdec.c
@@ -23,8 +23,8 @@
 # include "config.h"
 #endif
 
-#include <vlc_plugin.h>
 #include <vlc_common.h>
+#include <vlc_plugin.h>
 #include <vlc_codec.h>
 #include <vlc_messages.h>
 #include <ffnvcodec/dynlink_cuda.h>
